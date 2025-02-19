3aae5df0fe2eeac2c0ef95bba049cf71a79865c6
Aliza <59551300+sabeeka131@users.noreply.github.com>
2025-02-19 17:27:03 +0500

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,17 @@
+s1 = {2, 3, 1}
+s2 = {'b', 'a', 'c'}
+s3 = list(zip(s1, s2))
+print(s3,"\n")
+
+
+#zip elements of two lists
+#print elements one by one, but elemnts of 2nd list wil be in reverse order
+list1 = [10, 20, 30, 40]
+list2 = [100, 200, 300, 400]
+
+
+for x, y in zip(list1, list2[::-1]):
+    print(x, y)
+
+
+    
